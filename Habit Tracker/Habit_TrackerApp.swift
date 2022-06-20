b8ff39be86d33e5cc362743c9af87967f23b4867
Capslockhuh <jas.andrzejewski2008@gmail.com>
2022-06-20 15:03:42 +0200

    Initial Commit

---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Jan Andrzejewski on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
